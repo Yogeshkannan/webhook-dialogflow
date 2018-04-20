099f6c7a5cfde036b60a6b98ad3720e72b7b94cb
Yogesh <yogesh@10decoders.in>
2018-04-20 17:28:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('/get-employees', (req, res) => {
+
+    const reqUrl = 'http://localhost:9200/users/_search';
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const employees = JSON.parse(completeResponse);
+			console.log("Employeesss-->", employees)
+            let dataToSend = "Employees list coming soon..."
+
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-employees'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-employees'
+        });
+    });
+});
+
+server.listen((process.env.PORT || 8000), () => {
+    console.log("Server is up and running...");
+});
